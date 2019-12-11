e9356fa6eb20441e72e799b69138d2aef139da02
JosianeReis <josiane-reis@outlook.com>
2019-12-11 14:25:27 -0300

    Creating a simple component with styled-components

--- reactflix-context-api/src/App.tsx
+++ reactflix-context-api/src/App.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+import 'normalize.css'
+import 'milligram'
 
 const App: React.FC = () => (
-  <div className="App">
-    olar
+  <div>
+    <Header>
+      <h1>ReactFlix</h1>
+    </Header>
+    <main>
+      Conteudo
+    </main>
+    <Footer>
+      &copy; 2019
+    </Footer>
   </div>
 );
+
+const Header = styled.header`
+  height: 60px;
+  background: #333;
+`
+
+const Footer = styled.footer`
+  height: 30px;
+  background: #333;
+`
 
 export default App;
